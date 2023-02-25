c494ce68d9ab07cc7a7f3198d7c725fce98a29fb
tasyaa01 <annastasyaputriandra31@gmail.com>
2023-02-25 08:34:29 +0700

    api

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
 import 'package:get/get.dart';
+import 'package:mobile_project/app/data/entertainment_response.dart';
 import 'package:mobile_project/app/data/headline_response.dart';
+import 'package:mobile_project/app/data/sports_response.dart';
+import 'package:mobile_project/app/data/technology_response.dart';
 import 'package:mobile_project/app/utils/api.dart';
 
 class DashboardController extends GetxController {
@@ -10,8 +13,23 @@ class DashboardController extends GetxController {
   final _getConnect = GetConnect();
 
   Future<HeadlineResponse> getHeadline() async {
-    final response = await _getConnect.get(BaseUrl().headline);
+    final response = await _getConnect.get(BaseUrl.headline);
     return HeadlineResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<EntertainmentResponse> getEntertaiment() async {
+    final response = await _getConnect.get(BaseUrl.entertainment);
+    return EntertainmentResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<SportsResponse> getSports() async {
+    final response = await _getConnect.get(BaseUrl.sports); 
+    return SportsResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<TechnologyResponse> getTechnology() async {
+    final response = await _getConnect.get(BaseUrl.technology);
+    return TechnologyResponse.fromJson(jsonDecode(response.body));
   }
 
   final count = 0.obs;
